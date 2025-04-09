cddaa0be45e55f28c921e6379eeaf96a3dd00f86
kitmac03 <jkriego03@gmail.com>
2025-04-09 15:52:55 +0000

    awit

--- Vendor/editreq.blade.php
+++ Vendor/editreq.blade.php
@@ -3,7 +3,8 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="../css/editreq.css">
+  <link rel="stylesheet" href="../css/editreq.css">  <!-- wala pani nako gi convert ning css -->
+
   <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
